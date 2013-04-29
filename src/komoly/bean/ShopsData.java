697c58f2c61d91e07e09f446224b8be3e621143a
Zsu17 <rozslazac@vipmail.hu>
2013-04-29 22:47:25 +0300

    Create ShopsData.java

---
+++ src/komoly/bean/ShopsData.java
@@ -0,0 +1,37 @@
+package komoly.bean;
+
+public class ShopsData {
+  	int shopID, tel;
+		String name, address, contact;
+		public int getShopID() {
+			return shopID;
+		}
+		public void setShopID(int shopID) {
+			this.shopID = shopID;
+		}
+		public int getTel() {
+			return tel;
+		}
+		public void setTel(int tel) {
+			this.tel = tel;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getContact() {
+			return contact;
+		}
+		public void setContact(String contact) {
+			this.contact = contact;
+		}
+		
+}
